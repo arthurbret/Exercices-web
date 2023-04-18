1d71c652850be1310aac946a6cd87455797f1087
Arthur <arthur.bret@edu.ece.fr>
2023-04-18 10:52:30 +0200

    Système de note avec couleur interactive

--- 11_compteur/script.js
+++ 11_compteur/script.js
@@ -1,21 +1,31 @@
 const ajout = document.querySelector('#compteur-plus')
 const retrait = document.querySelector('#compteur-moins')
 const compteur = document.querySelector('#compteur')
-
-Number(compteur.innerHTML)
-
 const options = {
-    minValue: 0,
-    maxValue: 10,
+    minValue: 1,
+    maxValue: 20,
+    changeNote: 10,
 }
+
+compteur.innerHTML= options.minValue
+
+if (Number(compteur.innerHTML) > options.changeNote) compteur.classList.add('text-green-500')
+if (Number(compteur.innerHTML) <= options.changeNote) compteur.classList.add('text-red-500')
 
 ajout.addEventListener('click', () => {
     if (Number(compteur.innerHTML) < options.maxValue) compteur.innerHTML = Number(compteur.innerHTML) + 1
-    console.log(compteur.innerHTML)
+    if (Number(compteur.innerHTML) > options.changeNote){
+        compteur.classList.remove('text-red-500')
+        compteur.classList.add('text-green-500')
+    }
 })
 
 retrait.addEventListener('click', () => {
     if (Number(compteur.innerHTML) > options.minValue) compteur.innerHTML = Number(compteur.innerHTML) - 1
+    if (Number(compteur.innerHTML) <= options.changeNote){
+        compteur.classList.remove('text-green-500')
+        compteur.classList.add('text-red-500')
+    }
     console.log(compteur.innerHTML)
 })
 
