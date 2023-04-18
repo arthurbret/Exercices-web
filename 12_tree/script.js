f789a62ad4d5c0255b394d51bf1e2b2b29d90bec
Arthur <arthur.bret@edu.ece.fr>
2023-04-18 12:44:00 +0200

    Rien de nouveau

--- 12_tree/script.js
+++ 12_tree/script.js
@@ -4,6 +4,7 @@ let largeur = 28
 let block = `<div class="h-12 w-${largeur} bg-red-900 border border-black border-b-0"></div>`
 
 
+
 bouton.addEventListener('click', () => {
     conteneur.innerHTML += block
     largeur += 4
