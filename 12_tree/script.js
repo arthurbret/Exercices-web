5377a75782b9af41268d2cb110af4dec97729728
Arthur <arthur.bret@edu.ece.fr>
2023-04-18 12:36:54 +0200

    Ajout exercice 12

---
+++ 12_tree/script.js
@@ -0,0 +1,10 @@
+const bouton = document.querySelector('#bouton')
+const conteneur = document.querySelector('#block-conteneur')
+let largeur = 28
+let block = `<div class="h-12 w-${largeur} bg-red-900 border border-black border-b-0"></div>`
+
+
+bouton.addEventListener('click', () => {
+    conteneur.innerHTML += block
+    largeur += 4
+})
