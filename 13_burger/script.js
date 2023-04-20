216c36858f3333802b45e86e11dbe12f18d8cac1
Arthur <arthur.bret@edu.ece.fr>
2023-04-20 22:36:02 +0200

    Changement ordre texte js

--- 13_burger/script.js
+++ 13_burger/script.js
@@ -1,6 +1,6 @@
 import {burgerList} from './data.js'
 const container = document.querySelector('#burger-container')
-
+const bouton = document.querySelector('#bouton')
 
 // Voici la liste des burgers (le fichier se trouve dans le dossier data.js)
 console.log('burgerList', burgerList)
@@ -14,27 +14,6 @@ burgerList.forEach(burger => {
 
 // Etape 2
 // Dans #burger-container afficher le nom des burgers
-burgerList.forEach(burger => {
-    container.innerHTML += 
-`<div class="p-4 md:w-1/3">
-    <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-      <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="${burger.img}" alt="blog">
-      <div class="p-6">
-        <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">${burger.prix} €</h2>
-        <h1 class="title-font text-lg font-medium text-gray-900 mb-3">${burger.nom}</h1>
-        <p class="leading-relaxed mb-3">${burger.description}</p>
-        <div class="flex items-center flex-wrap">
-          <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">En savoir plus sur les ingédients
-            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M5 12h14"></path>
-              <path d="M12 5l7 7-7 7"></path>
-            </svg>
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>`
-})
 
 // Etape 3 
 // Ajouter la description et le prix 
@@ -51,6 +30,28 @@ burgerList.forEach(burger => {
 // Etape 7 (Bonus)
 // Rendre vos site le plus attractif possible en changeant le style
 // Vous pouvez remplacer les burgers par d'autre élements (ex: des films, des livres, des jeux vidéos, des personnages, des animaux, etc...)
+
+burgerList.forEach(burger => {
+    container.innerHTML += 
+`<div class="p-4 md:w-1/3">
+    <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+      <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="${burger.img}" alt="blog">
+      <div class="p-6">
+        <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">${burger.prix} €</h2>
+        <h1 class="title-font text-lg font-medium text-gray-900 mb-3">${burger.nom}</h1>
+        <p class="leading-relaxed mb-3">${burger.description}</p>
+        <div class="flex items-center flex-wrap">
+          <a id="bouton" class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">En savoir plus sur les ingédients
+            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+              <path d="M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>`
+})
 
 // Etape 8 (Bonus)
 // Afficher la liste des ingrédients
