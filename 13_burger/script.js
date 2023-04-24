4cc44ac4cf87faa05fb42c9fb92249ed395d24cd
Arthur <arthur.bret@edu.ece.fr>
2023-04-24 15:12:33 +0200

    Quelques tests

--- 13_burger/script.js
+++ 13_burger/script.js
@@ -1,37 +1,17 @@
 import {burgerList} from './data.js'
 const container = document.querySelector('#burger-container')
-const bouton = document.querySelector('#bouton')
+const bouton = document.querySelector('#myBtn')
+const boutonDroit = document.querySelector('#boutonDroit')
+const contenuModal = document.querySelector('#contenu-modal')
 
 // Voici la liste des burgers (le fichier se trouve dans le dossier data.js)
 console.log('burgerList', burgerList)
 
-
-// Etape 1
-// Pour chacun des burgers affiche le nom dans la console
 burgerList.forEach(burger => {
     console.log(burger.nom)
 })
 
-// Etape 2
-// Dans #burger-container afficher le nom des burgers
-
-// Etape 3 
-// Ajouter la description et le prix 
-
-// Etape 4 
-// Afficher l'image du burger
-
-// Etape 5
-// Ajouter des nouveaux burgers dans le fichier data.js
-
-// Etape 6 
-// Ajouter votre site sur github page et ajouter le lien dans le cours 
-
-// Etape 7 (Bonus)
-// Rendre vos site le plus attractif possible en changeant le style
-// Vous pouvez remplacer les burgers par d'autre élements (ex: des films, des livres, des jeux vidéos, des personnages, des animaux, etc...)
-
-burgerList.forEach(burger => {
+burgerList.forEach(function(burger, index){
     container.innerHTML += 
 `<div class="p-4 md:w-1/3">
     <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
@@ -41,7 +21,7 @@ burgerList.forEach(burger => {
         <h1 class="title-font text-lg font-medium text-gray-900 mb-3">${burger.nom}</h1>
         <p class="leading-relaxed mb-3">${burger.description}</p>
         <div class="flex items-center flex-wrap">
-          <a id="bouton" class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">En savoir plus sur les ingédients
+          <a id="but-ing" class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer">En savoir plus sur les ingrédients
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
               <path d="M12 5l7 7-7 7"></path>
@@ -52,6 +32,23 @@ burgerList.forEach(burger => {
     </div>
   </div>`
 })
+
+boutonDroit.addEventListener('click', () => {
+  window.open("https://www.example.com", "_blank", "width=200,height=100")
+})
+
+const butIng = document.querySelectorAll('#but-ing');
+const modal = document.querySelector('#myModal');
+
+butIng.forEach(burger, () =>{
+  burger.addEventListener('click', () => {
+    alert&(`tu clique sur ${burger.nom}`)
+    contenuModal.innerHTML = burger.ingredients;
+    modal.classList.remove('hidden');
+  });
+});
+
+
 
 // Etape 8 (Bonus)
 // Afficher la liste des ingrédients
