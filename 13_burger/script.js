1debf5eb03e018e828f67f65ba58a68618469e93
Arthur <arthur.bret@edu.ece.fr>
2023-04-25 11:31:11 +0200

    Plus d'ajout de tache vide et quelques améliorations

--- 13_burger/script.js
+++ 13_burger/script.js
@@ -10,7 +10,7 @@ burgerList.forEach(burger => {
     console.log(burger.nom)
 })
 
-burgerList.forEach(function(burger, index){
+burgerList.forEach(function(burger, index = 0){
     container.innerHTML += 
 `<div class="p-4 md:w-1/3">
     <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
@@ -20,7 +20,7 @@ burgerList.forEach(function(burger, index){
         <h1 class="title-font text-lg font-medium text-gray-900 mb-3">${burger.nom}</h1>
         <p class="leading-relaxed mb-3">${burger.description}</p>
         <div class="flex items-center flex-wrap">
-          <a id="ingredient-button" class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer">En savoir plus sur les ingrédients
+          <a id="ingredient-button-${index}" class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer">En savoir plus sur les ingrédients
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
               <path d="M12 5l7 7-7 7"></path>
@@ -30,6 +30,7 @@ burgerList.forEach(function(burger, index){
       </div>
     </div>
   </div>`
+  index +=1;
 })
 
 boutonDroit.addEventListener('click', () => {
@@ -39,7 +40,9 @@ boutonDroit.addEventListener('click', () => {
 const myBtn = document.querySelector('#myBtn');
 const modal = document.querySelector('#myModal');
 const span = document.querySelector('.close');
-const ingButton = document.querySelectorAll('#ingredient-button');
+for (let i = 0; i < burgerList.length; i++) {
+  const ingButton = document.querySelector(`#ingredient-button-${i}`);
+}
 
 myBtn.addEventListener('click', () => {
   modal.classList.remove('hidden');
@@ -48,6 +51,11 @@ myBtn.addEventListener('click', () => {
 span.addEventListener('click', () => {
   modal.classList.add('hidden');
 })
+
+ingButton.addEventListener('click', () => {
+  alert('tu clique sur le bouton')
+})
+
 ingButton.forEach(burger, () =>{
   burger.addEventListener('click', () => {
     alert(`tu clique sur ${burger.nom}`)
--- 14_to-do/script.js
+++ 14_to-do/script.js
@@ -10,9 +10,15 @@ const defaultToDo = [
 const displayTodos = (listTodo) => {
     listContainer.innerHTML = ''
     defaultToDo.forEach(element => {
-        listContainer.innerHTML += `<div class="border bg-slate-300 p-5 rounded-2xl">
-        
+        listContainer.innerHTML += `<div class="flex border bg-slate-300 p-5 rounded-2xl justify-between gap-10">
+        <div class="flex items-center gap-2">
+        <input id="checkbox" type="checkbox" ${element.done ? 'checked' : ''}>
         ${element.name}
+        </div>
+
+        <span id="bin-button" class="material-symbols-outlined cursor-pointer">
+            delete
+        </span>
         </div>`
     });
 }
@@ -30,12 +36,31 @@ displayTodos(defaultToDo)
 // #3 Ajouter un event listener sur le bouton pour ajouter un todo
 
 addButton.addEventListener('click', () => {
-    defaultToDo.push({done: false, name: input.value})
+    input.value ? defaultToDo.push({done: false, name: input.value}) : ''
     displayTodos(defaultToDo)
+    input.value = ''
+})
+
+checkbox.addEventListener('click', () => {
+    console.log('tu clique sur la checkbox')
 })
 
 // #4 (bonus) Faire un filtre
 
+
+
 // #5 (bonus) Supprimer un element
 
+const binButton = document.querySelector('#bin-button')
+
+binButton.addEventListener('click', () => {
+    alert('tu clique sur la poubelle')
+})
+
 // #6 (bonus) Cocher les checkbox pour marquer un todo comme fait
+
+const checkbox = document.querySelector('#checkbox')
+
+checkbox.addEventListener('click', () => {
+    console.log('tu clique sur la checkbox')
+})
