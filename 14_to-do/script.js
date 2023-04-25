b26542d107f2ec021f8cbece5c29a485292d364d
Arthur <arthur.bret@edu.ece.fr>
2023-04-25 12:49:47 +0200

    Ajout système de filtre check/non-check

--- 14_to-do/script.js
+++ 14_to-do/script.js
@@ -1,26 +1,67 @@
 const addButton = document.querySelector('#add')
 const listContainer = document.querySelector('#container-list')
 const input = document.querySelector('#new')
+const filterCheck = document.querySelector('#filter-checkbox')
+const filtre = document.querySelector('#filtre')
+const dataBaseUrl = "https://json-ece.glitch.me/data.json";
+
+fetch(dataBaseUrl)
+    .then(data => data.json())
+    .then(data => console.log(data))
 
 const defaultToDo = [
     {done: true, name: 'Learn HTML'},
     {done: true, name: 'Learn CSS'},
 ]
 
-const displayTodos = (listTodo) => {
+const displayTodos = (check) => {
     listContainer.innerHTML = ''
-    defaultToDo.forEach(element => {
-        listContainer.innerHTML += `<div class="flex border bg-slate-300 p-5 rounded-2xl justify-between gap-10">
-        <div class="flex items-center gap-2">
-        <input id="checkbox" type="checkbox" ${element.done ? 'checked' : ''}>
-        ${element.name}
-        </div>
-
-        <span id="bin-button" class="material-symbols-outlined cursor-pointer">
-            delete
-        </span>
-        </div>`
-    });
+    if (check === false) {
+        defaultToDo.forEach(element => {
+            if (element.done === false){
+                listContainer.innerHTML += `<div class="flex border bg-slate-300 p-5 rounded-2xl justify-between gap-10">
+            <div class="flex items-center gap-2">
+            <input id="checkbox" type="checkbox" ${element.done ? 'checked' : ''}>
+            ${element.name}
+            </div>
+    
+            <span id="bin-button" class="material-symbols-outlined cursor-pointer">
+                delete
+            </span>
+            </div>`
+            }
+        });
+    }
+    else if (check === true){
+        defaultToDo.forEach(element => {
+            if (element.done === true){
+                listContainer.innerHTML += `<div class="flex border bg-slate-300 p-5 rounded-2xl justify-between gap-10">
+            <div class="flex items-center gap-2">
+            <input id="checkbox" type="checkbox" ${element.done ? 'checked' : ''}>
+            ${element.name}
+            </div>
+    
+            <span id="bin-button" class="material-symbols-outlined cursor-pointer">
+                delete
+            </span>
+            </div>`
+            }
+        });
+    }
+    else if (check === 2){
+        defaultToDo.forEach(element => {
+            listContainer.innerHTML += `<div class="flex border bg-slate-300 p-5 rounded-2xl justify-between gap-10">
+            <div class="flex items-center gap-2">
+            <input id="checkbox" type="checkbox" ${element.done ? 'checked' : ''}>
+            ${element.name}
+            </div>
+    
+            <span id="bin-button" class="material-symbols-outlined cursor-pointer">
+                delete
+            </span>
+            </div>`
+        });
+    }
 }
 
 // #1 Selectionner le bouton avec l'id #add, le container #container-list, le input #new
@@ -31,30 +72,37 @@ addButton.addEventListener('click', () => {
 
 // #2 Afficher les todos dans le container avec une fonction displayTodos
 
-displayTodos(defaultToDo)
+displayTodos(2)
 
 // #3 Ajouter un event listener sur le bouton pour ajouter un todo
 
 addButton.addEventListener('click', () => {
     input.value ? defaultToDo.push({done: false, name: input.value}) : ''
-    displayTodos(defaultToDo)
+    displayTodos(filterCheck.checked)
     input.value = ''
-})
-
-checkbox.addEventListener('click', () => {
-    console.log('tu clique sur la checkbox')
 })
 
 // #4 (bonus) Faire un filtre
 
+filterCheck.addEventListener('click', () => {
+    displayTodos(filtre.checked ? filterCheck.checked : 2)
+})
 
+filtre.addEventListener('click', () => {
+    displayTodos(filtre.checked ? filterCheck.checked : 2)
+})
 
 // #5 (bonus) Supprimer un element
 
 const binButton = document.querySelector('#bin-button')
 
+const filterFunction = (listTodo) => {
+    binButton.value ? oui : non
+}
+
 binButton.addEventListener('click', () => {
     alert('tu clique sur la poubelle')
+
 })
 
 // #6 (bonus) Cocher les checkbox pour marquer un todo comme fait
